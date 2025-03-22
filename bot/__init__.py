ad900fab7b96f721854b199923fc8d3c34cc87cd
orgservice <146081596+orgservice@users.noreply.github.com>
2025-03-22 18:01:18 +0530

    Update __init__.py
    Signed-off-by: orgservice <146081596+orgservice@users.noreply.github.com>

--- bot/__init__.py
+++ bot/__init__.py
@@ -509,7 +509,7 @@ if len(AUTHOR_NAME) == 0:
 
 AUTHOR_URL = environ.get('AUTHOR_URL', '')
 if len(AUTHOR_URL) == 0:
-    AUTHOR_URL = 'https://t.me/AdultService'
+    AUTHOR_URL = 'https://t.me/AdultXPrime'
 
 TITLE_NAME = environ.get('TITLE_NAME', '')
 if len(TITLE_NAME) == 0:
