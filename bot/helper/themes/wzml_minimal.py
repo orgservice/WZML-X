c9e5fe8b7b062631f98743d6af4bcd69ca939e24
orgservice <146081596+orgservice@users.noreply.github.com>
2025-03-22 18:06:10 +0530

    Update wzml_minimal.py
    Signed-off-by: orgservice <146081596+orgservice@users.noreply.github.com>

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+2#!/usr/bin/env python3
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Updates'
+    ST_BN1_NAME = 'IMAX Prime'
     ST_BN1_URL = 'https://t.me/imaxprime'
-    ST_BN2_NAME = 'Adult Service'
-    ST_BN2_URL = 'https://t.me/AdultService'
+    ST_BN2_NAME = 'AdultXPrime'
+    ST_BN2_URL = 'https://t.me/adultservice'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
